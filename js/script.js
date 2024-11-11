f29d4adcc9b9e33121ee76a28408dc1d34b69a60
Marco Mechini <mechinimarco35@gmail.com>
2024-11-11 18:47:12 +0100

    feat: es complete

--- js/script.js
+++ js/script.js
@@ -2,44 +2,50 @@ const randArray = [];
 const userArray = [];
 const arrayNum = document.querySelector(".rand-num");
 const numForm = document.querySelector("form");
-const allUserNumber = document.querySelectorAll("type[number]");
+const allUserNumber = document.querySelectorAll("input[type='number']");
+const counterView = document.getElementById("counter");
+const correctNum = document.querySelector(".correct-num");
+const resultScore = document.getElementById("result-div");
+const resultNumber = document.getElementById("result");
 
 
 for (let i = 0; i < 5; i++) {
-    const randomNum = Math.floor(Math.random() * 9) + 1; //da 1 a 99
+    const randomNum = Math.floor(Math.random() * 99) + 1; //da 1 a 99
     randArray.push(randomNum);
-    arrayNum.innerHTML += `<div>${randomNum}</div>`
-    arrayNum.classList.add("show");
+    correctNum.innerHTML += `<div class="number">${randomNum}</div>`
 }
 
-console.log('Random array', randArray);
+//variabile che ad ogni intervallo decrementa di uno
+let counter = 30;
+const interval = setInterval(function () {
+    arrayNum.classList.remove("d-none");
+    counterView.classList.remove("d-none");
+    counterView.innerHTML = counter;
+    counter--;
+    //se il counter arriva a 0 vengono mostrati gli input
+    if (counter < 0) {
+        arrayNum.classList.add("d-none");
+        counterView.classList.add("d-none");
+        numForm.classList.remove("d-none")
+        clearInterval(interval);
+    }
+}, 1000);
 
 
 numForm.addEventListener("submit", function (event) {
     event.preventDefault();
-    console.log('entro?');
+
+    numForm.classList.add("d-none")
 
     for (let i = 0; i < allUserNumber.length; i++) {
         const curNumber = parseInt(allUserNumber[i].value);
         userArray.push(curNumber)
-        console.log('user array', userArray);
     }
-    console.log('a quanto pare non entro');
 
     let counter = 0;
     const commonNumber = [];
-    // const primo = parseInt(first.value);
-    // userArray.push(primo);
-    // const secondo = parseInt(second.value);
-    // userArray.push(secondo);
-    // const terzo = parseInt(third.value);
-    // userArray.push(terzo);
-    // const quarto = parseInt(four.value);
-    // userArray.push(quarto);
-    // const quinto = parseInt(five.value);
-    // userArray.push(quinto);
-    // console.log('User array', userArray);
 
+    //doppio ciclo per confrontare un valore alla volta di array random con tutti i valori dell'array composto dai numeri dell'utente
     for (let i = 0; i < randArray.length; i++) {
         const curRandomNumber = randArray[i];
         for (let j = 0; j < userArray.length; j++) {
@@ -52,10 +58,13 @@ numForm.addEventListener("submit", function (event) {
             }
         }
     }
-    console.log(`Hai azzeccato ${counter} numeri azzeccati sono: ${commonNumber}`);
 
+    resultScore.innerHTML += `<span>Hai azzeccato ${counter} numeri</span>`
+    for (let i = 0; i < commonNumber.length; i++) {
+        const element = commonNumber[i];
+
+        resultNumber.innerHTML += `<div class='checked'>${element}</div>`
+    }
     //disabilita il bottone al click
-    // document.querySelector("type[submmit]").disabled = true;
+    document.querySelector("button").disabled = true;
 })
-
-
